68d63fcc31817f54332b3c110ffda72e533bcc43
wf <wf_lv@sina.com>
2019-07-04 22:09:47 +0800

    更新

---
+++ resource/js/lang.js
@@ -0,0 +1,383 @@
+var oLang = {
+	"language": {
+		"cn": "language",
+		"en": "语言",
+	},
+	"peers": {
+		"cn": "节点",
+		"en": "peers",
+	},
+	"minting": {
+		"cn": "挖矿",
+		"en": "Minting"
+	},
+	"pmo": {
+		"cn": "代理挖矿专用",
+		"en": "Proxy Minters Only"
+	},
+	"address": {
+		"cn": "账户",
+		"en": "Address"
+	},
+	"enableForging": {
+		"cn": "授权子账户",
+		"en": "Enable Forging"
+	},
+	"peerList": {
+		"cn": "节点列表",
+		"en": "Peer List"
+	},
+	"refresh": {
+		"cn": "刷新列表",
+		"en": "Refresh"
+	},
+	"connectedPeers": {
+		"cn": "已连接列表",
+		"en": "Connected Peers"
+	},
+	"knownPeers": {
+		"cn": "可连接列表",
+		"en": "Known Peers"
+	},
+	"node": {
+		"cn": "节点",
+		"en": "node"
+	},
+	"ping": {
+		"cn": "延迟",
+		"en": "ping"
+	},
+	"state": {
+		"cn": "状态",
+		"en": "Status"
+	},
+	"version": {
+		"cn": "延迟",
+		"en": "version"
+	},
+	"operation": {
+		"cn": "操作",
+		"en": "Action"
+	},
+	"Connected": {
+		"cn": "已连接",
+		"en": "Connected"
+	},
+	"Delete": {
+		"cn": "删除节点",
+		"en": "Delete"
+	},
+	"Add": {
+		"cn": "添加节点",
+		"en": "Add"
+	},
+	"ManuallyAdd": {
+		"cn": "手动添加节点",
+		"en": "Manually Add"
+	},
+	"ManuallyRemove": {
+		"cn": "手动移除节点",
+		"en": "Manually Remove"
+	},
+	"ClearAllPeers": {
+		"cn": "清除全部可连接节点",
+		"en": "Clear All Peers"
+	},
+	"SynchroStatus": {
+		"cn": "区块同步状态",
+		"en": "Synchro Status"
+	},
+	"NodeVersion": {
+		"cn": "node UI 版本",
+		"en": "node UI Version"
+	},
+	"AuthorizeProxyMinting": {
+		"cn": "绑定账号",
+		"en": "Authorize Proxy Minting"
+	},
+	"MinterAddress": {
+		"cn": "授权地址",
+		"en": "Minter Address"
+	},
+	"ProxyMintingAddress": {
+		"cn": "被授权地址",
+		"en": "Proxy Minting Address"
+	},
+	"Ratio": {
+		"cn": "授权比例",
+		"en": "Ratio"
+	},
+	"Submit": {
+		"cn": "提交",
+		"en": "Submit"
+	},
+	"ProxyRelationship": {
+		"cn": "账户关系",
+		"en": "Proxy Relationship"
+	},
+	"NumOfBlocks": {
+		"cn": "出块数量",
+		"en": "# of Blocks"
+	},
+	"Stopped": {
+		"cn": "已停止",
+		"en": "Stopped"
+	},
+	"Active": {
+		"cn": "挖矿中",
+		"en": "Active"
+	},
+	"Modify": {
+		"cn": "修改",
+		"en": "Modify"
+	},
+	"Start": {
+		"cn": "开始挖矿",
+		"en": "Start"
+	},
+	"Stop": {
+		"cn": "停止挖矿",
+		"en": "Stop"
+	},
+	"CreateNewAddress": {
+		"cn": "创建账户",
+		"en": "Create New Address"
+	},
+	"Create": {
+		"cn": "创建账户",
+		"en": "Create"
+	},
+	"Clear": {
+		"cn": "清空",
+		"en": "Clear"
+	},
+	"Mnemonic": {
+		"cn": "生成的助记词为",
+		"en": "Mnemonic"
+	},
+	"Address2": {
+		"cn": "生成的地址为",
+		"en": "Address"
+	},
+	"AddYourAddress": {
+		"cn": "添加账户",
+		"en": "Add Your Address"
+	},
+	"InputMnemonic": {
+		"cn": "助记词",
+		"en": "Input Mnemonic"
+	},
+	"SetPassword": {
+		"cn": "请输入密码",
+		"en": "Set Password"
+	},
+	"ConfirmPassword": {
+		"cn": "再次确认密码",
+		"en": "Confirm Password"
+	},
+	"AddSub": {
+		"cn": "添加账户",
+		"en": "Add"
+	},
+	"AddressList": {
+		"cn": "账户列表",
+		"en": "Address List"
+	},
+	"Address3": {
+		"cn": "地址",
+		"en": "Address"
+	},
+	"ActivationStatus": {
+		"cn": "激活状态",
+		"en": "Activation Status"
+	},
+	"Remove": {
+		"cn": "移除账户",
+		"en": "Remove"
+	},
+	"Activated": {
+		"cn": "已激活",
+		"en": "Activated"
+	},
+	"Unactivated": {
+		"cn": "未激活",
+		"en": "Unactivated"
+	},
+	"PaymentTransfer": {
+		"cn": "转账",
+		"en": "Payment/Transfer"
+	},
+	"SenderAddress": {
+		"cn": "转出地址",
+		"en": "Sender Address"
+	},
+	"ChooseAddress": {
+		"cn": "选择转出地址",
+		"en": "Choose address"
+	},
+	"AssetType": {
+		"cn": "转账种类",
+		"en": "Asset Type"
+	},
+	"RecipientAddress": {
+		"cn": "收款地址",
+		"en": "Recipient Address"
+	},
+	"Amount": {
+		"cn": "转账金额",
+		"en": "Amount"
+	},
+	"Fee": {
+		"cn": "转账手续费",
+		"en": "Fee"
+	},
+	"Send": {
+		"cn": "确定",
+		"en": "Send"
+	},
+	"Level": {
+		"cn": "级别",
+		"en": "Level"
+	},
+	"Genesis": {
+		"cn": "创世",
+		"en": "Genesis"
+	},
+	"LevelReached": {
+		"cn": "已达标",
+		"en": "Level Reached"
+	},
+	"LevelUnreached": {
+		"cn": "未达标",
+		"en": "Level Unreached"
+	},
+	"EnableForging": {
+		"cn": "授权",
+		"en": "Enable Forging"
+	},
+	"Tier2": {
+		"cn": "二代",
+		"en": "Tier 2"
+	},
+	"Submitted": {
+		"cn": "已提交",
+		"en": "Submitted"
+	},
+	"Confirm": {
+		"cn": "确定",
+		"en": "Confirm"
+	},
+	"AddedSuccessfully": {
+		"cn": "添加成功",
+		"en": "Added Successfully"
+	},
+	"RemovedSuccessfully": {
+		"cn": "移除成功",
+		"en": "Removed Successfully"
+	},
+	"PeerRemoved": {
+		"cn": "该节点已移除",
+		"en": "Peer removed"
+	},
+	"WrongPassword": {
+		"cn": "密码错误",
+		"en": "Wrong Password"
+	},
+	"Text1": {
+		"cn": "请把助记词的地址按顺序记录在纸上并保持好，需要此助记词才能访问你的账户地址",
+		"en": "Record the mnemonic words in the right order on a piece of paper and keep them well. You will need the mnemonic to access your address."
+	},
+	"Text2": {
+		"cn": "注意不要泄露你的助记词，如果你失去了它，你就无法进入你的账户地址",
+		"en": "Attention! Do not disclose your mnemonic. If you lose it, you will have no way to access your address."
+	},
+	"Text3": {
+		"cn": "密码长度为8-24，至少包含数字，大写英文，小写英文和特殊符号中的三种",
+		"en": "Password length is 8-24, including number, upper and lower case letters and special symbol. （At least have 3 types of those）"
+	},
+	"Text4": {
+		"cn": "输入待授权地址",
+		"en": "Input Address to be Enabled"
+	},
+	"Text5": {
+		"cn": "被授权地址不能为空",
+		"en": "Enabled address cannot be blank"
+	},
+	"Text6": {
+		"cn": "授权地址或被授权地址未激活",
+		"en": "Enabling address or enabled address not activated"
+	},
+	"Text7": {
+		"cn": "原因可能如下：<br/>1.没有足够的MCF激活手续费",
+		"en": " Possible Reasons：<br/>1.Not enough MCF to activate the address"
+	},
+	"Text8": {
+		"cn": "激活失败!",
+		"en": "Activation Failed!"
+	},
+	"Text9": {
+		"cn": "激活请求已提交",
+		"en": "Activation request sent"
+	},
+	"Text10": {
+		"cn": "请选择地址",
+		"en": "Please choose the address."
+	},
+	"Text11": {
+		"cn": "账号未激活",
+		"en": "address not activated"
+	},
+	"Text12": {
+		"cn": "转账请求已提交",
+		"en": "Payment/transfer request sent"
+	},
+	"Text13": {
+		"cn": "原因可能如下：<br/>1.上一笔交易还未被区块确认<br/>2.当前转账资产种类余额不足<br/>3.没有足够的MCF转账手续费",
+		"en": "Possible Reasons：<br/>1.Last transaction has not been confirmed<br/>2.Insufficient asset balance<br/>3.Insufficient MCF balance"
+	},
+	"Text14": {
+		"cn": "转账失败",
+		"en": "Payment/transfer failed"
+	},
+	"Text15": {
+		"cn": "助记词密码不能为空",
+		"en": "Password cannot be blank"
+	},
+	"Text16": {
+		"cn": "密码不符合规则，请重新输入",
+		"en": "Password does not meet requirement. Please input again"
+	},
+	"Text17": {
+		"cn": "两次密码不相同，请重新输入",
+		"en": "Passwords do not match. Please input again."
+	},
+	"Text18": {
+		"cn": "助记词错误！",
+		"en": "Wrong Mnemonic!"
+	},
+	"Text19": {
+		"cn": "未添加过账户",
+		"en": "No addressed added!"
+	},
+	"Text20": {
+		"cn": "请输入密码",
+		"en": "Please input password."
+	},
+	"Text21": {
+		"cn": "请输入节点",
+		"en": "Please input peer"
+	},
+	"Text22": {
+		"cn": "全部移除成功",
+		"en": "All peers removed successfully"
+	},
+	"Text23": {
+		"cn": "无可移除节点",
+		"en": " No peer to remove"
+	}
+}
+
+
+function getLang(v){
+	return oLang[v][langFlag];
+}
